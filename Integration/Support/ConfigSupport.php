5a68531ad66f3b360cffd1cb40523dceaf7cabe3
Jason Gerber <jason@jasongerber.ch>
2025-07-18 10:46:11 +0200

    fix?

--- Integration/Support/ConfigSupport.php
+++ Integration/Support/ConfigSupport.php
@@ -6,7 +6,6 @@ namespace MauticPlugin\GrapesJsBlockLoaderBundle\Integration\Support;
 
 use Mautic\IntegrationsBundle\Integration\DefaultConfigFormTrait;
 use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormInterface;
-use MauticPlugin\GrapesJsBlockLoaderBundle\Integration\GrapesJsBlockLoaderIntegration;
 
 class ConfigSupport extends GrapesJsBlockLoaderIntegration implements ConfigFormInterface
 {
